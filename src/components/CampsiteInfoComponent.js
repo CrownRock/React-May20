bd549a970bc7569d9770b7d5ac11f9c7f0445d71
Steve Stout <sstout7576@gmail.com>
2020-04-29 11:13:55 -0600

    week3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,28 +24,30 @@ class CampsiteInfo extends Component {
                {comments.map(comment => {
                   return (
                      <div>
-                        <p>{ comment.text }</p> 
-                        <p>{ comment.author }, 
+                        <p>{comment.text}</p> 
+                        <p>{comment.author}, 
                         { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date))) }</p>
                      </div>
-                  );
-               })
+                     );
+                  })
                };
             </div>
          );
-      }
-      return <div />      
+      }      
    }
 
    render() {
       if (this.props.campsite) {
-         return (<div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-         </div>
+         return (
+            <div className="container">
+               <div className="row">
+               {this.renderCampsite(this.props.campsite)}
+               {this.renderComments(this.props.campsite.comments)}
+               </div>
+            </div>
          );
       }
-         return <div />
+       return <div />
       };
    }
 
